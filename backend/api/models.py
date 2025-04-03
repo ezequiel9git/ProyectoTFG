e7ff831b11c859290180f22a2ca00a6c1d32af71
ezequiel9git <ezequielparradolopez@gmail.com>
2025-04-03 21:24:08 +0200

    Credenciales de base de datos en setting.py. Creación de modelos en models.py y completado de migraciones.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=15)
+    direccion = models.TextField() 
+    asunto = models.TextField() # Descripción del problema enfrentado
+    prioridad = models.TextField() # Prioridad de seguimiento
+    terapeuta = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el terapeuta
+
+
+    # Método STR para identificar al usuario por su nombre (self.nombre)
+    def __str__(self):
+        return self.nombre
+
+
+
+class Sesion(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name="sesiones")
+    fecha = models.DateTimeField(auto_now_add=True)
+    evaluación = models.TextField() # Informe de evaluación de la sesión
+    actividades = models.TextField() # Tareas programadas para el paciente
+    proximasesion = models.TextField() # Anotaciones para la próxima sesión
+
+    # Método STR para identificar sesión en base al paciente y fecha de sesión
+    def __str__(self):
+        return f"Sesión de {self.paciente.nombre} el {self.fecha.strftime('%d-%m-%Y')}"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,12 +76,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ezequiel',
+        'PASSWORD': 'Ezequiel12345-',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
